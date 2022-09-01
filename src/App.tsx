b14455a4573ae57d43524b1ab6773f289fa9a154
Diego Fernando Rojas Carrillo <drawingsdf@gmail.com>
2022-09-01 18:09:50 -0500

    updeated commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
+import { useState } from "react";
+import { Task } from "./interfaces/Task";
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
 
-function App() {
+interface Props {
+  title?: string;
+}
+
+function App({ title }: Props) {
+  const [tasks, setTasks] = useState<Task[]>([
+    {
+      id: 1,
+      title: "Learn react",
+      description: "Learn react",
+      completed: false,
+    },
+  ]);
+
+  const getCurrentTimestamp = (): any => new Date().getTime();
+
+  const newANewTask = (task: Task) =>
+    setTasks([
+      ...tasks,
+      { ...task, id: getCurrentTimestamp(), completed: false },
+    ]);
+
+  const DeleteATask = (id: number) =>
+    setTasks(tasks.filter((task) => task.id !== id));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-dark text-white" style={{ height: "100vh" }}>
+      <nav className="navbar navbar-dark bg-primary">
+        <div className="container">
+          <a href="/" className="navbar-brant">
+            <img style={{ width: "4rem" }} src={logo} alt="react logo" />
+          </a>
+          {title}
+        </div>
+      </nav>
+      <main className="container p-4">
+        <div>
+          <div className="col-md-12 pb-4">
+            <TaskForm newANewTask={newANewTask} />
+          </div>
+
+          <div className="col-md-12">
+            <div className="row">
+              <TaskList tasks={tasks} DeleteATask={DeleteATask} />
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
